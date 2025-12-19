4ca8654f75345a1f1c31f1aae0750806bf9fc671
BunnyBae <07112006kz@mail.ru>
2025-12-19 19:58:16 +0300

    Add arguments and format option to CLI

--- gendiff.js
+++ gendiff.js
@@ -7,11 +7,15 @@ const program = new Command();
 program
   .name("gendiff")
   .description("Compares two configuration files and shows a difference.")
-  .version("1.0.0");
+  .version("1.0.0")
+  .argument("<filepath1>", "path to first file")
+  .argument("<filepath2>", "path to second file")
+  .option("-f, --format <type>", "output format", "stylish")
+  .action((filepath1, filepath2, options) => {
+    console.log(`File 1: ${filepath1}`);
+    console.log(`File 2: ${filepath2}`);
+    console.log(`Format: ${options.format}`);
+    console.log("TODO: Compare files and show difference");
+  });
 
 program.parse(process.argv);
-
-// Вывод справки, если не переданы аргументы
-if (!process.argv.slice(2).length) {
-  program.help();
-}
